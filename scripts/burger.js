a39ea55a25e17af1b5f1ace7707ed4fb212aa364
MarkHW <lolka-pop@rambler.ru>
2022-09-19 04:24:04 +0300

    это все на что я способен сегодня...

---
+++ scripts/burger.js
@@ -0,0 +1,4 @@
+document.getElementById('button').onclick = function() {
+    document.getElementById('burger').classList.toggle('active');
+    document.getElementById('button').classList.toggle('reverse');
+  }
